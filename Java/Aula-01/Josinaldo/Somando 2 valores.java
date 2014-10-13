a088b8884fcebe1f4ace3e7e31933d200de068a3
Josinaldo Albuquerque Barbosa <josinaldo.web@gmail.com>
2014-10-13 16:31:08 -0300

    Create Somando 2 valores.java

---
+++ Java/Aula-01/Josinaldo/Somando 2 valores.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HelloWorld
+{
+
+    public static void main(String []args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        int number1;
+        int number2;
+        int sum;
+        
+        System.out.println(sum);
+    }
+}
