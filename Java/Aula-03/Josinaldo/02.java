c0d32f4d7e1e99b51291f09ea291516a0519f3b9
Josinaldo Albuquerque Barbosa <josinaldo.web@gmail.com>
2014-10-13 12:02:48 -0300

    Criado 02.java
    for do tipo "lista"

---
+++ Java/Aula-03/Josinaldo/02.java
@@ -0,0 +1,15 @@
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+	
+		int[] numeros = {1,2,3,4,5,6,7,8,9,10};
+		
+		for (int item : numeros)
+		{
+		System.out.println("Contador: " + item);
+		}
+
+	}
+
+}
