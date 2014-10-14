f39210b0805ad1f09eed2613e47a2c9d519adf2e
Josinaldo Albuquerque Barbosa <josinaldo.web@gmail.com>
2014-10-14 11:23:46 -0300

    alterando o if

--- C/Aula-03/Josinaldo/prog4.c
+++ C/Aula-03/Josinaldo/prog4.c
@@ -7,9 +7,8 @@ main() {
     do {
     printf("Entre com um mes valido: ");
     scanf("%d", &mes);
-        if ((mes < 1) || (mes > 12)){
+        if ((mes < 1) || (mes > 12))
             printf("\nErro: mes invalido\n");
-        }
     } while ((mes < 1) || (mes > 12));
 
 }
