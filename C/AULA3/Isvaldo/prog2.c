84b46a9a543218958dac12588de9b6fb2c5c7af5
Isvaldo Fernandes de souza <isvaldo.fernandes@gmail.com>
2014-10-18 19:11:36 -0300

    Create prog2.c

---
+++ C/AULA3/Isvaldo/prog2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*Faça um algoritmo que calcule a soma dos números de 1 a 100*/
+
+main () {
+int n =0;
+int i;
+
+for (i=1; i<101;i++){
+        n +=i;
+}
+printf("Total da soma %d",n);
+}
