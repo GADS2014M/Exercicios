3ffb41a0302ad24f8b32cfad8a6bc42fe9257e6a
Josinaldo Albuquerque Barbosa <josinaldo.web@gmail.com>
2014-10-19 17:44:33 -0200

    Olámundo!

---
+++ Java/AULA1/Josinaldo/Olámundo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+
+	/**
+	 * @author Josinaldo Albuquerque Barbosa
+	 */
+	
+	public static void main(String[] args) {
+		
+	  	System.out.print("Olá, mundo!");
+		
+	}
+	
+}
