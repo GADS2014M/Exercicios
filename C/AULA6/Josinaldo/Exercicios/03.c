6cc43f2a92c547116b1c4572d78be429dd048f4f
Josinaldo Albuquerque Barbosa <josinaldo.barbosa@al.infnet.edu.br>
2014-11-26 23:59:05 -0200

    Create 03.c

---
+++ C/AULA6/Josinaldo/Exercicios/03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main(){
+
+    public boolean verificaVetor(int vetor, numero){
+        boolean resultado;
+        for(int i = 0; i<vetor; i++){
+            vetor[i];
+            if(vetor[i] == numero){
+                resultado == true;
+            else{
+                resultado == false;
+            }
+            }
+        }
+    }
+
+    int vetor = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    
+    printf()
+
+}
