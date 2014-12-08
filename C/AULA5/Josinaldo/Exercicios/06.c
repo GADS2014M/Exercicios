c327b4b530b22c582ee7891ccfb5c5f0a154806a
Josinaldo Albuquerque Barbosa <josinaldo.barbosa@al.infnet.edu.br>
2014-12-08 00:43:06 -0200

    Create 06.c

---
+++ C/AULA5/Josinaldo/Exercicios/06.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+    6. Faça um programa que leia um nome no formato “nome sobrenome” e mostre no formato “sobrenome, nome”.
+*/
+
+main(){
+
+    char nome_completo  [100];
+    char nome           [50];
+    char sobrenome      [50];
+    int i = 0, j = 0;
+
+    puts("Por favor, digite nome e sobrenome: ");
+    gets(nome_completo);
+
+    // Pega nome
+    while((nome_completo[i] != ' ') && (nome_completo[i] != '\0')){
+        nome[i] = nome_completo[i];
+        i++;
+    }
+
+    // Pra pular o espaço
+    i++;
+
+    // Pega sobrenome
+    while(nome_completo[i] != '\0'){
+        sobrenome[j] = nome_completo[i];
+        j++;
+        i++;
+    }
+
+    nome[i] = '\0';
+    sobrenome[j] = '\0';
+
+    printf("%s, %s", sobrenome, nome);
+
+}
