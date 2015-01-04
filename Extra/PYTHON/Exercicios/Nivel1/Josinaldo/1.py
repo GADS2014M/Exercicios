1ae0d8b12ab73642cf05bb048e372c7a06a80e4d
Josinaldo Albuquerque Barbosa <josinaldo.barbosa@al.infnet.edu.br>
2015-01-04 17:25:25 -0200

    Create 1.py

---
+++ Extra/PYTHON/Exercicios/Nivel1/Josinaldo/1.py
@@ -0,0 +1,14 @@
+# coding: latin-1
+
+'''
+Created on 04/01/2015
+
+@author: Josinaldo
+
+# 1) Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+'''
+
+if __name__ == '__main__':
+    n1 = int (raw_input("Digite o primeiro número: "))
+    n2 = int (raw_input("Digite o segundo número: "))
+    print "A soma deles é: %d" % (n1 + n2)
