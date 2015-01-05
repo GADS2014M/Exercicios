cf080854c71fb98e394be2b1852a52098e6d0a17
Josinaldo Albuquerque Barbosa <josinaldo.barbosa@al.infnet.edu.br>
2015-01-05 19:02:20 -0200

    Create 2.py

---
+++ Extra/PYTHON/Exercicios/Nivel1/Josinaldo/2.py
@@ -0,0 +1,10 @@
+# coding: latin-1
+
+'''
+@author: Josinaldo
+# 2) Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+'''
+
+if __name__ == '__main__':
+    n1 = int (raw_input("Digite o valor em metros: "))
+    print "O valor em milímetros é: %d" % (n1 / 0.0010000)
